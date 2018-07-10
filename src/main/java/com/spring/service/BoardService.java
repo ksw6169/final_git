cf826f8940dd50bb370f1d4f46c67e708391835d
zzblely <wpdms95@gmail.com>
2018-07-10 12:10:59 +0900

    수정

--- src/main/java/com/spring/service/BoardService.java
+++ src/main/java/com/spring/service/BoardService.java
@@ -2,6 +2,7 @@ package com.spring.service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
@@ -52,34 +53,33 @@ public class BoardService {
 		}
 		mav.addObject("msg", msg);
 		return mav;
-
-	//내가 쓴 글 리스트
-	public HashMap<String, Object> myWriteList(Map<String, String> params) {
-		HashMap<String, Object> map = new HashMap<String, Object>(); //담아서 보낼 HashMap 생성
-		inter = sqlSession.getMapper(BoardInter.class);
-		/*int sNum = Integer.parseInt(params.get("sNum"));
-		int eNum = Integer.parseInt(params.get("eNum"));*/
-		ArrayList<BoardDTO> list = inter.myWriteList(params); //ArrayList로 해당 아이디가 쓴 글 담기
-		logger.info(""+list.size());
-		int listCnt = inter.myWriteListCnt(params); //페이징을 위해 글 갯수 가져옴
-		map.put("list", list); //보낼 리스트를 담음
-		map.put("listCnt", listCnt); //글 갯수 담음
-		return map;
 	}
 
-	//내가 쓴 댓글리스트
-	public HashMap<String, Object> myReplyList(Map<String, String> params) {
-		HashMap<String, Object> map = new HashMap<String, Object>();//담아서 보낼 HashMap 생성
-		inter = sqlSession.getMapper(BoardInter.class);
-		ArrayList<BoardDTO> list = inter.myReplyList(params);//ArrayList로 해당 아이디가 쓴 글 담기
-		logger.info(""+list.size());
-		int listCnt = inter.myReplyListCnt(params);//페이징을 위해 글 갯수 가져옴
-		map.put("list", list);//보낼 리스트를 담음
-		map.put("listCnt", listCnt);//글 갯수 담음
-		return map;
-	}
-	
-	}
+		//내가 쓴 글 리스트
+		public HashMap<String, Object> myWriteList(Map<String, String> params) {
+			HashMap<String, Object> map = new HashMap<String, Object>(); //담아서 보낼 HashMap 생성
+			inter = sqlSession.getMapper(BoardInter.class);
+			/*int sNum = Integer.parseInt(params.get("sNum"));
+			int eNum = Integer.parseInt(params.get("eNum"));*/
+			ArrayList<BoardDTO> list = inter.myWriteList(params); //ArrayList로 해당 아이디가 쓴 글 담기
+			logger.info(""+list.size());
+			int listCnt = inter.myWriteListCnt(params); //페이징을 위해 글 갯수 가져옴
+			map.put("list", list); //보낼 리스트를 담음
+			map.put("listCnt", listCnt); //글 갯수 담음
+			return map;
+		}
+
+		//내가 쓴 댓글리스트
+		public HashMap<String, Object> myReplyList(Map<String, String> params) {
+			HashMap<String, Object> map = new HashMap<String, Object>();//담아서 보낼 HashMap 생성
+			inter = sqlSession.getMapper(BoardInter.class);
+			ArrayList<BoardDTO> list = inter.myReplyList(params);//ArrayList로 해당 아이디가 쓴 글 담기
+			logger.info(""+list.size());
+			int listCnt = inter.myReplyListCnt(params);//페이징을 위해 글 갯수 가져옴
+			map.put("list", list);//보낼 리스트를 담음
+			map.put("listCnt", listCnt);//글 갯수 담음
+			return map;
+		}
 
 	public ModelAndView nBoardDetail(String board_no) {
 		logger.info("공지사항 상세보기 서비스");
@@ -131,6 +131,10 @@ public class BoardService {
 		mav.setViewName(page);
 		return mav;
 	}
+
+
+
+
 
 
 	
