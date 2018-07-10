6dbfadd2550ee6dee9a127abbbb33369aa8cc4ca
yuna <yuna932@naver.com>
2018-07-10 11:12:10 +0900

    회원 수정 폼, 회원수정 완료, 마이페이지 서브메뉴바 수정

--- src/main/java/com/spring/dao/MemberInter.java
+++ src/main/java/com/spring/dao/MemberInter.java
@@ -1,9 +1,13 @@
 package com.spring.dao;
 
+import java.util.HashMap;
+
 import com.spring.dto.MemberDTO;
 
 public interface MemberInter {
 
 	int join(MemberDTO dto);	// 회원가입 요청
 	MemberDTO login(String id);			// 로그인 요청
+	MemberDTO member(String userId);	//회원정보
+	int perUpdate(HashMap<String, String> map);	//회원 수정
 }
